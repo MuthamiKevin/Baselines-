89bbdf8c155bbcdf5ac0a75ca377a10244dc50bc
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-07 00:33:05 +0300

    update

--- FireApp/urls.py
+++ FireApp/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('FirePlay/', include('FirePlay.urls'))
+    path('FireApp/', include('FireApp.urls'))
 ]
