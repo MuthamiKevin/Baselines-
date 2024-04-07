652d4e29e9a3039744a75dfcd6eb14b740cc4fc1
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-07 19:48:02 +0300

    updae

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound    
+from django.views.decorators.http import require_http_methods  
 
 # Create your views here.
-def index(request):
+def say_hello(request):
     return HttpResponse('Hello world')
