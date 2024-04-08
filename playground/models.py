ffa4a0b82eead73dc99daeb8b7d4d88a07ec5606
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-08 13:30:49 +0300

    update

--- playground/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Feature(models.Model):
-    id: int
-    name: str
-    details: str
--- playground/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('playground/', views.say_hello)
-   
-]
