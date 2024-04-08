2cd473ae85227cd753a750d32e10de57b5098209
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-08 11:43:36 +0300

    update

--- playground/models.py
+++ playground/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Feature(models.Model):
+    id: int
+    name: str
+    details: str
--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound    
 from django.views.decorators.http import require_http_methods  
-from .models import  Feature
+from .models import Feature
 # Create your views here.
 def say_hello(request):
     feature1 = Feature()
@@ -10,4 +10,4 @@ def say_hello(request):
     feature1.detail = 'Our service are very quick'
     
     
-    return render(request, 'hello.html', {'feature': feature1})
+    return render(request, 'hello.html', {'Feature': feature1})
